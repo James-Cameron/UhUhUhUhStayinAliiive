b271c56b03f30119cbe75418293f013205acfb8a
James-Cameron <jharoldcameron@gmai.com>
2020-04-19 18:02:37 -0400

    Added music, main menu

---
+++ UhUhUhUhStayinAliiive/Assets/_JAMES/SCRIPTS/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+
+
+    public void LoadJvac()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadStayinAlive()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+
+
+    // END OF FILE
+}
